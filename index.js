c68e1ac9abd8ca6f18465c9e9e3c5ed38da82217
methreban2023 <alrefai.dalia@gmail.com>
2023-05-18 17:27:40 +0300

    employees and managers assignment

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-const employeesJSON = require('./employees.json');
-const managersJSON = require('./managers.json');
+const employeesJSON = require("./employees.json");
+const managersJSON = require("./managers.json");
 
-// Code Here
+class Employee {
+  constructor(name, position, yearJoined, salary) {
+    this.name = name;
+    this.position = position;
+    this.yearJoined = yearJoined;
+    this.salary = salary;
+  }
+
+  idBadge() {
+    let pos = `< ${this.position} : ${this.name}`;
+    return pos;
+  }
+}
+class Manager extends Employee {
+  // this.emp = [];
+  constructor(name, position, yearJoined, salary, bonusPercentage) {
+    super(name, position, yearJoined, salary);
+    this.bonusPercentage = bonusPercentage;
+  } // constructor
+
+  salaryIncrease(increaseAmount) {
+    this.salary = this.salary + increaseAmount;
+  }
+  logManagerInfo() {
+    //   <name>
+    //  since <workingYears>
+    //  Bonus % <bonusPercentage></bonusPercentage>
+    console.log(this.name);
+    console.log(`since ${2023 - this.yearJoined}`);
+    console.log(`Bonus % ${this.bonusPercentage} / ${this.bonusPercentage}`);
+  }
+} // Manager
+
+const employees = new Employee(employeesJSON.map(emp));
+const managers = new Manager(managersJSON.map());
