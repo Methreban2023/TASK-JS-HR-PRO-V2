b2dd0146b4cafb0d1599378e97a134a46a81db8c
methreban2023 <alrefai.dalia@gmail.com>
2023-05-20 10:48:03 +0300

    employees and managers classes may 29th

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ class Employee {
   }
 
   idBadge() {
-    let pos = `< ${this.position} : ${this.name}`;
+    let pos = ` ${this.position}: ${this.name}`;
     return pos;
   }
 }
 class Manager extends Employee {
-  // this.emp = [];
+  //this.emp[]
   constructor(name, position, yearJoined, salary, bonusPercentage) {
     super(name, position, yearJoined, salary);
     this.bonusPercentage = bonusPercentage;
@@ -24,12 +24,13 @@ class Manager extends Employee {
   salaryIncrease(increaseAmount) {
     this.salary = this.salary + increaseAmount;
   }
+
   logManagerInfo() {
     //   <name>
     //  since <workingYears>
     //  Bonus % <bonusPercentage></bonusPercentage>
     console.log(this.name);
-    console.log(`since ${this.getWorkingYears}`);
+    console.log(`since ${this.getWorkingYears()}`);
     console.log(`Bonus % ${this.bonusPercentage} / ${this.bonusPercentage}`);
   }
   getWorkingYears() {
@@ -38,11 +39,43 @@ class Manager extends Employee {
   }
 } // Manager
 
-// const employees = new Employee(employeesJSON.map(emp));
-// const managers = new Manager(managersJSON.map());
-const employees[];
-const managers[];
-employeesJSON.map((emp)=> employees.push(emp));
-managersJSON.map((manage)=> managers.push(manage));
+const employees = employeesJSON.map((emp) => {
+  //console.log(emp);
+  return new Employee(emp.name, emp.position, emp.yearJoined, emp.salary);
+  //return emp;
+});
+const managers = managersJSON.map((manage) => {
+  return new Manager(
+    manage.name,
+    manage.position,
+    manage.yearJoined,
+    manage.salary,
+    manage.bonusPercentage
+  );
+});
+console.log(employees);
+employees.forEach((emp) => console.log(emp.idBadge()));
+const superHeroes = employees.filter((emp) => emp.position === "Developer");
+console.log(superHeroes);
 
+managers.map((manage) => {
+  if (manage.getWorkingYears() > 11) manage.salaryIncrease(1000);
+});
+console.log(managers);
 
+managers.forEach((manage) => manage.logManagerInfo());
+
+const seniorExecutives = managers.filter((manage) => {
+  if (manage.yearJoined < 2012) return manage.name;
+});
+console.log(seniorExecutives);
+
+const lowestBonus = managers.find((manage) => {
+  if (manage.bonusPercentage === 0.1) return manage;
+});
+console.log(lowestBonus);
+
+const promotionCandidate = employees.filter((emp) => {
+  if (emp.salary > 8000) return emp;
+});
+console.log(promotionCandidate);
